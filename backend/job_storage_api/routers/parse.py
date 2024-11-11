938689e0510a1d6afa105c47e459041b7ad4f3d2
ionic101 <iv.osipov2004@gmail.com>
2024-11-12 00:04:15 +0500

    Add pattern of parsing routes

--- backend/job_storage_api/routers/parse.py
+++ backend/job_storage_api/routers/parse.py
@@ -1,13 +1,30 @@
 from fastapi import APIRouter
 from job_storage_api.schemas.vacancies_filter import VacanciesFilter
 import requests
+from typing import Dict
 
 
 parse_router = APIRouter(prefix='/parse', tags=['Parse'])
-URL: str = 'https://api.hh.ru/vacancies'
+URLS: Dict[str, str] = {
+    'hh': 'https://api.hh.ru/vacancies',
+    'rabota': '',
+    'avito': ''
+}
 
+
+@parse_router.get('/hh')
+def search_vacancies_hh(request: VacanciesFilter):
+    response = requests.get(URLS['hh'], data=request.dict())
+    return response.json()
+
+@parse_router.get('/rabota')
+def search_vacancies_rabota(request: VacanciesFilter):
+    return {}
+
+@parse_router.get('/avito')
+def search_vacancies_avito(request: VacanciesFilter):
+    return {}
 
 @parse_router.get('/')
 def search_vacancies(request: VacanciesFilter):
-    response = requests.get(URL, data=request.dict())
-    return response.json()
+    return search_vacancies_hh(request) | search_vacancies_rabota(request) | search_vacancies_avito(request)
